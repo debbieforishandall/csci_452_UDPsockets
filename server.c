9453841fb0c510af3680236d671eb80e58659687
root <root@localhost.localdomain>
2017-02-12 22:23:22 -0500

    Minor: fixed servaddr

--- server.c
+++ server.c
@@ -209,7 +209,7 @@ int main(int argc, char *argv[]) {
 
 				memset(&si_tcp, 0, sizeof(si_tcp));
 				si_tcp.sin_family      = AF_INET;
-				si_tcp.sin_addr.s_addr = htonl(si_other);
+				si_tcp.sin_addr.s_addr = si_other.sin_addr.s_addr;
 				si_tcp.sin_port        = htons(tcp_port);
 
 				if ( (connect(conn_s, (struct sockaddr *) &si_other, sizeof(si_other) )) < 0 ) {
