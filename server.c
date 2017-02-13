48da41eda880ad82875d8f4be90042862e8f410f
root <root@localhost.localdomain>
2017-02-12 21:08:38 -0500

    Remove sock reuse option

--- server.c
+++ server.c
@@ -209,7 +209,7 @@ int main(int argc, char *argv[]) {
 				/*  Bind our socket addresss to the 
 					listening socket, and call listen()  */
 
-				int yes = 1;
+				//int yes = 1;
 				/*  Create the listening socket  */
 
 				tcp_list_s = socket(AF_INET, SOCK_STREAM, 0);
@@ -218,10 +218,10 @@ int main(int argc, char *argv[]) {
 					exit(EXIT_FAILURE);
 				}
 
-				if (setsockopt(tcp_list_s, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) {
+				/*if (setsockopt(tcp_list_s, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) {
 					perror("setsockopt");
 					exit(1);
-				}
+				}*/
 
 				if ( bind(tcp_list_s, (struct sockaddr *) &si_tcp, sizeof(si_tcp)) < 0 ) {
 					fprintf(stderr, "ECHOSERV: Error calling bind()\n");
@@ -272,7 +272,7 @@ int main(int argc, char *argv[]) {
 		else if(strcmp(msg_type, "QUIT\n") == 0)
 		{
 			//Writeline(conn_s, "Oops, An error occured", 23);
-			close(list_s);
+			//close(list_s);
 		}
 	}
 	
